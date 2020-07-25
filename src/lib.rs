220503ff22fdcbb642ed209ff32a2ec2b637739f
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-07-25 23:10:25 +0200

    Split from framework

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(feature="array", allow(incomplete_features),feature(const_generics,maybe_uninit_extra,maybe_uninit_uninit_array))]
+#![cfg_attr(feature="timeout", feature(thread_spawn_unchecked,duration_zero))]
+#![cfg_attr(feature="vector", feature(iterator_fold_self))]
+
+//#[macro_export] macro_rules! dbg { ( $first:expr $(,$A:expr)* ) => ( eprint!("{} = {:?}", stringify!($first), $first); $( eprint!(", {} = {:?}", stringify!($A), $A); )* eprintln!(""); ) }
+pub mod error; pub use error::{Error, Result/*bail, ensure, Ok*/}; #[cfg(feature="fehler")] pub use error::throws;
+pub use cfg_if::cfg_if;
+pub mod iter;
+pub mod slice;
+#[cfg(feature="array")] pub mod array; //pub use array::{Iterator, map};
+pub mod num; pub use num::{Zero, Ratio, abs};
+cfg_if! { if #[cfg(feature="vector")] { #[macro_use] pub mod vector; pub use vector::{Bounds, MinMax}; }} //xy, int2, uint2, size, vec2
+pub fn time<T>(task: impl FnOnce() -> T) -> T {
+	let start = std::time::Instant::now();
+	let result = task();
+	eprintln!("{}", start.elapsed().as_millis());
+	result
+}
+cfg_if! { if #[cfg(feature="trace")] { mod trace; pub use trace::rstack_self; }}
+cfg_if! { if #[cfg(feature="timeout")] { mod timeout; pub use timeout::timeout; }}
+#[cfg(feature="signal-hook")] pub use trace::sigint_trace;
+pub use core::{ops, result, convert}; // derive_more, num_enum
